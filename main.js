33a731149dfdb768267367234f64104e4d94286a
Augusto <argus@MacBook-Air-de-Augusto.local>
2024-05-03 11:37:19 -0300

    mudancas layout

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const fsImg = require('fs');
 const axios = require('axios');
+const http = require('http');
+const https = require('https');
 const puppeteer = require('puppeteer');
 const readline = require('readline');
 const { app, BrowserWindow, ipcMain, dialog } = require('electron');
@@ -54,7 +56,7 @@ async function renderPageToImage(url, filename) {
 
   // Adjust viewport
   await page.setViewport({ width: 800, height: 600 });
-  
+
   await page.screenshot({ path: filename, fullPage: true });
 
   await browser.close();
@@ -62,67 +64,56 @@ async function renderPageToImage(url, filename) {
 
 
 }
-ipcMain.on('processaLink', async (event, [url, _id,_pagina]) => {
-  var temlinha=lines.indexOf(url.trim())
-  console.log("temlinha", temlinha);
+
+ipcMain.on('processaLink', async (event, [url, _id, _pagina, _salva]) => {
+  var temlinha = lines.indexOf(url.trim())
+  console.log("temlinha", url, temlinha, _salva);
   //console.log(url, 'SCREENSHOTS/' +nomePDF+ _pagina + ".jpg");
 
-  axios.get(url)
-    .then(response => {
-      /*
-      fs.writeFile('SCREENSHOTS/'+_id+".png", response.data, (err) => {
-        if (err) throw err;
-        console.log('Image downloaded successfully!');
-      });
-      */
-      // Check if the response status code indicates a successful request
-      if (response.status === 200) {
-        // Response contains the HTML content
-        const html = response.data;
-
-        // You can save the HTML source code to a file if needed
-        //fs.writeFileSync('webpage.html', html);
-
-        console.log('Web page loaded successfully!');
-        console.log('HTML content:');
-        console.log(html.length);
-        if (html.length > 10) {
-          event.sender.send('QRCodeProcessado', ["url ok!", _id,temlinha]);
-          output.write(url + ";" + _id + ";" + "url ok!\n");
-
+  if (isValidUrl(url)) {
+    axios.get(url)
+      .then(response => {
+        if (response.status === 200) {
+          const html = response.data;
+          if (html.length > 10) {
+            event.sender.send('QRCodeProcessado', ["url ok!", _id, temlinha]);
+            output.write(url + ";" + _id + ";" + "url ok!\n");
+            if (_salva) {
+              renderPageToImage(url, 'SCREENSHOTS/' + nomePDF + '_pagina_' + _pagina + ".jpg")
+                .catch(error => console.error('Error rendering page to image:', error));
+            }
+          } else {
+            event.sender.send('QRCodeProcessado', ["link vazio...", _id, temlinha]);
+            output.write(url + ";" + _id + ";" + "link vazio...\n");
+          }
         } else {
-          event.sender.send('QRCodeProcessado', ["link vazio...", _id,temlinha]);
-          output.write(url + ";" + _id + ";" + "link vazio...\n");
+          console.log('Failed to load the web page. Status code: ' + response.status);
+          event.sender.send('QRCodeProcessado', ["url com erro...", _id, temlinha]);
+          output.write(url + ";" + _id + ";" + "url com erro..\n");
         }
-      } else {
-        console.log('Failed to load the web page. Status code: ' + response.status);
-        event.sender.send('QRCodeProcessado', ["url com erro...", _id,temlinha]);
-        output.write(url + ";" + _id + ";" + "url com erro..\n");
-      }
-    })
-    .catch(error => {
-      event.sender.send('QRCodeProcessado', [error, _id,temlinha]);
-      output.write(url + ";" + _id + ";" + "talvez nao seja QRCODE...\n");
-    })
-
-    if(isValidUrl(url)){
-      renderPageToImage(url, 'SCREENSHOTS/'+nomePDF+'_pagina_' + _pagina + ".jpg")
-      .catch(error => console.error('Error rendering page to image:', error));
-    }
+      })
+      .catch(error => {
+        event.sender.send('QRCodeProcessado', [error, _id, temlinha]);
+        output.write(url + ";" + _id + ";" + "talvez nao seja QRCODE...\n");
+      })
+  } else {
+    event.sender.send('QRCodeProcessado', ["Link invalido", _id, temlinha]);
+    output.write(url + ";" + _id + ";" + "link invalido\n");
+  }
 
 })
 ipcMain.on('abrearquivoTxt', async (event, nome) => {
 
-  readFileToArray("TXT/"+nome)
-  .then(lines => {
-    //console.log('Array of lines:', lines);
-  })
-  .catch(err => {
-    //console.error('Error reading file:', err);
-  });
+  readFileToArray("TXT/" + nome)
+    .then(lines => {
+      //console.log('Array of lines:', lines);
+    })
+    .catch(err => {
+      //console.error('Error reading file:', err);
+    });
 });
 ipcMain.on('abrearquivo', async (event, nome) => {
-  nomePDF=nome.substring(0, nome.lastIndexOf("."));
+  nomePDF = nome.substring(0, nome.lastIndexOf("."));
   output = fs.createWriteStream("PDF/" + nomePDF + ".txt");
   output.once('open', function (fd) {
     output.write("URL;ID;Resultado:\n");
@@ -136,7 +127,7 @@ ipcMain.on('fim', async (event, oque) => {
 
 function readFileToArray(filePath) {
   return new Promise((resolve, reject) => {
-    
+
     const rl = readline.createInterface({
       input: fs.createReadStream(filePath),
       crlfDelay: Infinity
